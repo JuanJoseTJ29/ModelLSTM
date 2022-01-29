630c5e72a716d097f73c29167e5409c9d9de1f61
mishellgc <mishell.gomez@unmsm.edu.pe>
2022-01-29 06:31:52 -0500

    añadiendo métricas y actualizando estilos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, model, model2, model3 #, modelRF  # import your app modules here
+from apps import home, model, model2, model3 , modelRF  # import your app modules here
 
 app = MultiApp()
 
@@ -13,7 +13,7 @@ st.markdown("""
 app.add_app("Home", home.app)
 app.add_app("Modelo LSTM", model.app)
 app.add_app("Modelo SVR", model2.app)
-# app.add_app("Modelo Random Forest", modelRF.app)
+app.add_app("Modelo Random Forest", modelRF.app)
 app.add_app("Modelo SVC", model3.app)
 # The main app
 app.run()
