4c73212b4c57baf248f788ac432021958e991f9f
JuanJoseTJ29 <50315174+JuanJoseTJ29@users.noreply.github.com>
2022-01-29 07:48:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, model, model2, model3 , modelRF  # import your app modules here
+from apps import home, model, model3 , modelRF  # import your app modules here model2
 
 app = MultiApp()
 
@@ -12,7 +12,7 @@ st.markdown("""
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Modelo LSTM", model.app)
-app.add_app("Modelo SVR", model2.app)
+#app.add_app("Modelo SVR", model2.app)
 app.add_app("Modelo Random Forest", modelRF.app)
 app.add_app("Modelo SVC", model3.app)
 # The main app
