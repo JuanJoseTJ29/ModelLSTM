cd08c17bd4f9dfac6413f17d66f1888a791f9ab3
JuanJoseTJ29 <50315174+JuanJoseTJ29@users.noreply.github.com>
2022-01-29 06:53:08 -0500

    Update modelRF.py

--- apps/modelRF.py
+++ apps/modelRF.py
@@ -1,122 +1,68 @@
-import yfinance as yf
+from tensorflow.keras.models import load_model
+import streamlit as st
 import talib
-import plotly
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import pyplot
+from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import ParameterGrid
-from sklearn import metrics
-%matplotlib inline
-import streamlit as st
+from sklearn.metrics import roc_auc_score, plot_roc_curve
+from sklearn.metrics import roc_curve, auc
+import plotly.express as px
 
 
 def app():
     st.title('Model - Random Forest')
 
+    start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
+    end = st.date_input('End' , value=pd.to_datetime('today'))
 
-    ticker= "googl"
-    stock_data = yf.download(ticker, start="2008-01-04", end="2022-01-27")
 
+    st.title('Predicción de tendencia de acciones')
 
-    fig = plt.figure(figsize = (12,6))
-    stock_data['Adj Close'].plot()
-    plt.ylabel("Precio de cierre ajustado")
-    st.pyplot(fig)
-    
-    fig2 = plt.figure(figsize = (12,6))
-    stock_data['Adj Close'].pct_change().plot.hist(bins=50)
-    plt.xlabel("Cambio porcentual de cierre ajustado de 1 día")
-    st.pyplot(fig2) 
-    
-    feature_names = []
-    for n in [14, 30, 50, 200]:
-        stock_data['ma' + str(n)] = talib.SMA(stock_data['Adj Close'].values, timeperiod=n)
-        stock_data['rsi' + str(n)] = talib.RSI(stock_data['Adj Close'].values, timeperiod=n)
+	
 
-        feature_names = feature_names + ['ma' + str(n), 'rsi' + str(n)]
+    # Visualizaciones
+    st.subheader('Precio de cierre ajustado')
+    fig = px.line(stock_data,y='Adj Close')
+    st.plotly_chart(fig)
 
-    stock_data['Volume_1d_change'] = stock_data['Volume'].pct_change()
-
-    volume_features = ['Volume_1d_change']
-    feature_names.extend(volume_features)
-    
-    stock_data['5d_future_close'] = stock_data['Adj Close'].shift(-5)
-    stock_data['5d_close_future_pct'] = stock_data['5d_future_close'].pct_change(5)
-    
-    # Visualizando datos
-    st.subheader('Mostrar los datos') 
-    st.write(stock_data.head())
-
-    stock_data.dropna(inplace=True)
-
-    X = stock_data[feature_names]
-    y = stock_data['5d_close_future_pct']
-
-    train_size = int(0.85 * y.shape[0])
-    X_train = X[:train_size]
-    y_train = y[:train_size]
-    X_test = X[train_size:]
-    y_test = y[train_size:]
-    
-    grid = {'n_estimators': [200], 'max_depth': [3], 'max_features': [4, 8], 'random_state': [42]}
-    test_scores = []
-
-    rf_model = RandomForestRegressor()
-
-    for g in ParameterGrid(grid):
-        rf_model.set_params(**g) 
-        rf_model.fit(X_train, y_train)
-        test_scores.append(rf_model.score(X_test, y_test))
-
-    best_index = np.argmax(test_scores)
-    
-    rf_model = RandomForestRegressor(n_estimators=200, max_depth=3, max_features=4, random_state=42)
-    rf_model.fit(X_train, y_train)
-
+    st.subheader('Cambio porcentual de cierre ajustado de 1 día')
+    fig = plt.figure(figsize=(12, 6))
+	
     y_pred = rf_model.predict(X_test)
+    st.subheader('Porcentaje de cambio de precio de cierre previsto de 5 días')
+    y_pred_series = pd.Series(y_pred, index=y_test.index)
+    fig = px.line(y_pred_series)
+    st.plotly_chart(fig)
 
 
-    y_pred_series = pd.Series(y_pred, index=y_test.index)   
+    # Evaluación del modelo
 
-
-    st.subheader('Grafica de cambio de precio')
-    fig3 = plt.figure(figsize = (12,6))
-    y_pred_series.plot()
-    plt.ylabel("Porcentaje de cambio de precio de cierre previsto de 5 días")
-    st.pyplot(fig3)
-
-
-    from sklearn import metrics
+    st.title('Evaluación del Modelo RFR')
+    ## Métricas
     MAE=metrics.mean_absolute_error(y_test, y_pred)
     MSE=metrics.mean_squared_error(y_test, y_pred)
     RMSE=np.sqrt(metrics.mean_squared_error(y_test, y_pred))
-    #print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-    #print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-    #print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-
-
-    metrics = {
-    'metrics' : ['Mean Absolute Error', 'Mean Squared Error', 'Root Mean Squared Error'],
-    'valor': [MAE, MSE, RMSE]
+    metricas = {
+        'metrica' : ['Mean Absolute Error', 'Mean Squared Error', 'Root Mean Squared Error'],
+        'valor': [MAE, MSE, RMSE]
     }
-    metrics = pd.DataFrame(metrics)
-
-    import plotly.express as px
-
-    fig = px.bar(metrics, x="metrics", y="valor", color="metrics",text_auto=True,title="Métricas del modelo Random Forest ")
-
+    metricas = pd.DataFrame(metricas)  
     
-    importances = rf_model.feature_importances_
-    sorted_index = np.argsort(importances)[::-1]
-    x_values = range(len(importances))
-    labels = np.array(feature_names)[sorted_index]
-    
-    st.subheader('Grafica ma')
-    fig5 = plt.figure(figsize = (12,6))
-    plt.bar(x_values, importances[sorted_index], tick_label=labels)
-    plt.xticks(rotation=90)
-    plt.legend()
-    st.pyplot(fig)
-    
+    ### Gráfica de las métricas
+    st.subheader('Métricas de rendimiento') 
+    fig = px.bar(        
+        metricas,
+        x = "metrica",
+        y = "valor",
+        title = "Métricas del Modelo Random Forest Regressor",
+        color="metrica"
+    )
+    st.plotly_chart(fig)
+
+
+    ## Curva ROC
+
+    #ax = plt.gca()
+    #rfc_disp = plot_roc_curve(rf_model, X_test, y_test, ax=ax, alpha=0.8)
+    #plt.show()
+    #st.pyplot(fig)
